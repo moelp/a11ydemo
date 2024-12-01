5289cf80dd0edc5af72398f82543aaa50cee7e0c
moelp <dev@moelp.com>
2024-12-01 18:50:40 +0100

    fix(useDarkMode): event type

--- src/utils/useDarkMode.tsx
+++ src/utils/useDarkMode.tsx
@@ -7,16 +7,13 @@ function useDarkMode() {
   );
 
   useEffect(() => {
-    // Create a media query listener for dark mode
     const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
 
-    // Define a handler that updates the state based on the media query
-    const handleChange = (event) => setIsDarkMode(event.matches);
+    const handleChange = (event: MediaQueryListEvent) =>
+      setIsDarkMode(event.matches);
 
-    // Add event listener for media query changes
     mediaQuery.addEventListener('change', handleChange);
 
-    // Cleanup event listener on component unmount
     return () => mediaQuery.removeEventListener('change', handleChange);
   }, []);
 
