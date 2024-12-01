88cccc13cd402a95687ae21dff46185a8c48c657
Moris <lauingerm@mediamarktsaturn.com>
2024-12-01 18:05:41 +0100

    chore: add tailwind support

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,59 @@
-import type { Config } from "tailwindcss";
+import { createContainer } from '#/utils/createContainer';
+import type { Config } from 'tailwindcss';
+import plugin from 'tailwindcss/plugin';
+import containerQueries from '@tailwindcss/container-queries';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    screens: {
+      sm: '40em',
+      // => @media (min-width: 40em) { ... } -> 640px
+
+      md: '48em',
+      // => @media (min-width: 48em) { ... } -> 768px
+
+      lg: '64em',
+      // => @media (min-width: 64em) { ... } -> 1024px
+
+      xl: '80em',
+      // => @media (min-width: 80em) { ... } -> 1280px
+
+      '2xl': '96em',
+      // => @media (min-width: 96em) { ... } -> 1536px
+    },
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primaryLighter: 'var(--primary--lighter)',
+        primary: 'var(--primary)',
+        primaryHover: 'var(--primary--hover)',
+        background: 'var(--background)',
+        backgroundSecondary: 'var(--background--secondary)',
+        backgroundLevel1: 'var(--background--level1)',
+        backgroundLevel1Hover: 'var(--background-level1-hover)',
+        foreground: 'var(--foreground)',
+        foregroundMuted: 'var(--foreground--muted)',
+        foregroundMutedOnSecondary: 'var(--foreground--muted--onSecondary)',
+        foregroundPrimary: 'var(--foreground--primary)',
+        foregroundOnPrimary: 'var(--foreground--onPrimary)',
+        foregroundLevel1Hover: 'var(--foreground-level1-hover)',
+      },
+      gridTemplateColumns: {
+        // Simple 16 column grid
+        autowrap: 'repeat(auto-fit, minmax(20rem, 1fr))',
       },
     },
   },
-  plugins: [],
+  plugins: [
+    containerQueries,
+    // plugin(function ({ addVariant }) {
+    //   addVariant('darkmode', '&:data-mode:"dark"');
+    //   addVariant('lightmode', '&:data-mode:"light"');
+    // }),
+  ],
 };
 export default config;
