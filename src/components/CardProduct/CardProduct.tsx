272f81ff2a5ee55f446fa0540736ee19d5f77f99
Moris <lauingerm@mediamarktsaturn.com>
2024-12-01 18:13:09 +0100

    feat(component): Add CardProduct

---
+++ src/components/CardProduct/CardProduct.tsx
@@ -0,0 +1,213 @@
+import Button from '#/components/Button/Button';
+import { useA11yContext } from '#/utils/a11yContext';
+
+type CardProductProps = {
+  isA11y: boolean;
+};
+
+export default function CardProduct({ isA11y }: CardProductProps) {
+  const { state: a11yMode } = useA11yContext();
+  isA11y = isA11y != undefined ? isA11y : a11yMode.isSemantic;
+
+  return (
+    <div className="rounded-lg border border-gray-200 bg-white p-6 shadow-sm dark:border-gray-700 dark:bg-gray-800">
+      <div className="h-56 w-full">
+        <a href="#">
+          <img
+            className="mx-auto h-full dark:hidden"
+            src="https://flowbite.s3.amazonaws.com/blocks/e-commerce/imac-front.svg"
+            alt=""
+          />
+          <img
+            className="mx-auto hidden h-full dark:block"
+            src="https://flowbite.s3.amazonaws.com/blocks/e-commerce/imac-front-dark.svg"
+            alt=""
+          />
+        </a>
+      </div>
+      <div className="pt-6">
+        <div className="mb-4 flex items-center justify-between gap-4">
+          <span className="me-2 rounded bg-blue-100 px-2.5 py-0.5 text-xs font-medium text-blue-800 dark:bg-blue-900 dark:text-blue-300">
+            {' '}
+            Up to 35% off{' '}
+          </span>
+
+          <div className="flex items-center justify-end gap-1">
+            <Button
+              isA11y={isA11y}
+              variant="ghost"
+              iconOnly={true}
+              onClick={() => alert('clicked quick look')}>
+              {isA11y && <span className="sr-only"> Quick look </span>}
+              <svg
+                className="h-5 w-5"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="none"
+                viewBox="0 0 24 24">
+                <path
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  d="M21 12c0 1.2-4.03 6-9 6s-9-4.8-9-6c0-1.2 4.03-6 9-6s9 4.8 9 6Z"></path>
+                <path
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"></path>
+              </svg>
+            </Button>
+            <Button
+              isA11y={isA11y}
+              variant="ghost"
+              iconOnly={true}
+              onClick={() => alert('clicked add to Favorites')}>
+              <span className="sr-only"> Add to Favorites </span>
+              <svg
+                className="h-5 w-5"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24">
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M12 6C6.5 1 1 8 5.8 13l6.2 7 6.2-7C23 8 17.5 1 12 6Z"></path>
+              </svg>
+            </Button>
+          </div>
+        </div>
+
+        <a
+          href="#"
+          className="text-lg font-semibold leading-tight text-gray-900 hover:underline dark:text-white">
+          {'Apple iMac 27", 1TB HDD, Retina 5K Display, M3 Max'}
+        </a>
+
+        <div className="mt-2 flex items-center gap-2">
+          <div className="flex items-center">
+            <svg
+              className="h-4 w-4 text-yellow-400"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 24 24">
+              <path d="M13.8 4.2a2 2 0 0 0-3.6 0L8.4 8.4l-4.6.3a2 2 0 0 0-1.1 3.5l3.5 3-1 4.4c-.5 1.7 1.4 3 2.9 2.1l3.9-2.3 3.9 2.3c1.5 1 3.4-.4 3-2.1l-1-4.4 3.4-3a2 2 0 0 0-1.1-3.5l-4.6-.3-1.8-4.2Z"></path>
+            </svg>
+
+            <svg
+              className="h-4 w-4 text-yellow-400"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 24 24">
+              <path d="M13.8 4.2a2 2 0 0 0-3.6 0L8.4 8.4l-4.6.3a2 2 0 0 0-1.1 3.5l3.5 3-1 4.4c-.5 1.7 1.4 3 2.9 2.1l3.9-2.3 3.9 2.3c1.5 1 3.4-.4 3-2.1l-1-4.4 3.4-3a2 2 0 0 0-1.1-3.5l-4.6-.3-1.8-4.2Z"></path>
+            </svg>
+
+            <svg
+              className="h-4 w-4 text-yellow-400"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 24 24">
+              <path d="M13.8 4.2a2 2 0 0 0-3.6 0L8.4 8.4l-4.6.3a2 2 0 0 0-1.1 3.5l3.5 3-1 4.4c-.5 1.7 1.4 3 2.9 2.1l3.9-2.3 3.9 2.3c1.5 1 3.4-.4 3-2.1l-1-4.4 3.4-3a2 2 0 0 0-1.1-3.5l-4.6-.3-1.8-4.2Z"></path>
+            </svg>
+
+            <svg
+              className="h-4 w-4 text-yellow-400"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 24 24">
+              <path d="M13.8 4.2a2 2 0 0 0-3.6 0L8.4 8.4l-4.6.3a2 2 0 0 0-1.1 3.5l3.5 3-1 4.4c-.5 1.7 1.4 3 2.9 2.1l3.9-2.3 3.9 2.3c1.5 1 3.4-.4 3-2.1l-1-4.4 3.4-3a2 2 0 0 0-1.1-3.5l-4.6-.3-1.8-4.2Z"></path>
+            </svg>
+
+            <svg
+              className="h-4 w-4 text-yellow-400"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 24 24">
+              <path d="M13.8 4.2a2 2 0 0 0-3.6 0L8.4 8.4l-4.6.3a2 2 0 0 0-1.1 3.5l3.5 3-1 4.4c-.5 1.7 1.4 3 2.9 2.1l3.9-2.3 3.9 2.3c1.5 1 3.4-.4 3-2.1l-1-4.4 3.4-3a2 2 0 0 0-1.1-3.5l-4.6-.3-1.8-4.2Z"></path>
+            </svg>
+          </div>
+
+          <p className="text-sm font-medium text-gray-900 dark:text-white">
+            5.0
+          </p>
+          <p className="text-sm font-medium text-gray-500 dark:text-gray-400">
+            (455)
+          </p>
+        </div>
+
+        <ul className="mt-2 flex items-center gap-4">
+          <li className="flex items-center gap-2">
+            <svg
+              className="h-4 w-4 text-gray-500 dark:text-gray-400"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24">
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M13 7h6l2 4m-8-4v8m0-8V6a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1v9h2m8 0H9m4 0h2m4 0h2v-4m0 0h-5m3.5 5.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0Zm-10 0a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0Z"></path>
+            </svg>
+            <p className="text-sm font-medium text-gray-500 dark:text-gray-400">
+              Fast Delivery
+            </p>
+          </li>
+
+          <li className="flex items-center gap-2">
+            <svg
+              className="h-4 w-4 text-gray-500 dark:text-gray-400"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24">
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeWidth="2"
+                d="M8 7V6c0-.6.4-1 1-1h11c.6 0 1 .4 1 1v7c0 .6-.4 1-1 1h-1M3 18v-7c0-.6.4-1 1-1h11c.6 0 1 .4 1 1v7c0 .6-.4 1-1 1H4a1 1 0 0 1-1-1Zm8-3.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z"></path>
+            </svg>
+            <p className="text-sm font-medium text-gray-500 dark:text-gray-400">
+              Best Price
+            </p>
+          </li>
+        </ul>
+
+        <div className="mt-4 flex items-center justify-between gap-4">
+          <p className="text-2xl font-extrabold leading-tight text-gray-900 dark:text-white">
+            $1,699
+          </p>
+          <Button
+            isA11y={isA11y}
+            variant="ghost"
+            onClick={() => alert('clicked add to cart')}>
+            <svg
+              className="-ms-2 me-2 h-5 w-5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="none"
+              viewBox="0 0 24 24">
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 4h1.5L8 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm.75-3H7.5M11 7H6.312M17 4v6m-3-3h6"></path>
+            </svg>
+            <span>Add to cart</span>
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
