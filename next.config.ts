27d8fd0eb7cc30b0a23971246a830ebcace45538
moelp <dev@moelp.com>
2024-12-01 23:05:39 +0100

    chore(nextjs): Remove static build as it is not compatiible with app atm

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,9 @@ import type { NextConfig } from 'next';
 import createMDX from '@next/mdx';
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '/a11ydemo',
-  distDir: 'dist',
+  //output: 'export',
+  //basePath: '/a11ydemo',
+  //distDir: 'dist',
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   sassOptions: {
     silenceDeprecations: ['legacy-js-api'],
